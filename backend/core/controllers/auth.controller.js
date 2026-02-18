4b6f8ddffbcf8c03fe04ecabcc062cf503f30486
alegimlop <alegimlop@alu.edu.gva.es>
2026-02-18 17:09:10 +0100

    creacion de registro

--- backend/core/controllers/auth.controller.js
+++ backend/core/controllers/auth.controller.js
@@ -1,4 +1,25 @@
+import { Prisma } from "@prisma/client";
+
 const authService = require("../services/auth.service");
+const bcrypt = require("bcrypt");
+
+export async function registername(req, res) {
+  try {
+    const { name, username, email, password } = req.body
+    const hashedPassword  = await bcrypt.hash(password, 11)
+    const user = await prisma.player.create({
+      data: { name, username, email, password: hashedPassword }
+    })
+    res.status(201).json({ id: user.id, name: user.name, username: user.username, email: user.email })
+  } catch (error) {
+    if (error.code === "P2002") {
+      return res.status(400).json({
+        error: "Usuario o email ya existe"
+      })
+    }
+    res.status(500).json({ message: error.message })
+  }
+}
 
 exports.loginCliente = async (req, res) => {
   try {
