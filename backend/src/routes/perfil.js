ce7c23e1d0eba86355a44a32b5429314a52ea228
alegimlop <alegimlop@alu.edu.gva.es>
2026-04-20 20:46:19 +0200

    borrado de perfil funcional

--- backend/src/routes/perfil.js
+++ backend/src/routes/perfil.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getPerfil, updatePerfil } = require('../controllers/perfilController');
+const { getPerfil, updatePerfil, deletePerfil } = require('../controllers/perfilController');
 
+router.delete('/:id', deletePerfil);
 router.get('/:id', getPerfil);
 router.put('/:id', updatePerfil);
 
